f30f8748df76ccfbb203f95396894751d6c329a6
David Alamu <aestrex@gmail.com>
2019-02-02 02:45:20 +0100

    initialize passport

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.sendFile('auth.html', {
     root: __dirname
   });
+});
+
+app.get('/success', (req, res) => {
+  res.send('You have successfully logged in');
+});
+
+app.get('/error', (req, res) => {
+  res.send('error loggin in');
+});
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(user, cb) {
+  cb(null, obj);
 });
 
 const port = process.env.PORT || 3000;
